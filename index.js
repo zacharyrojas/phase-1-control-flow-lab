44923edccf1be902de51a080d07e1d0143e82612
Zachary Rojas <ztr2194@gmail.com>
2023-03-17 16:56:56 -0500

    control flow lab done

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if (x <= 400) {
+   return x = 'This one is on me!';
+  } else if (x <= 2000) {
+    return x = 'That will be twenty bucks.'
+  } else if (x <= 2500) {
+    return x = "I will gladly take your thirty bucks."
+  } else {return x = "No can do."}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(x){
+  const result = x === 'NYC' ? "Ok, sounds good." : "No go.";
+  return result;
+} 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(x){
+  switch (x) {
+    case 'generous':
+      return 'Thank you so much.';
+      
+    case 'not as generous':
+      return 'Thank you.';
+      
+    case 'thanks for everything':
+      return 'Bye.'
+    
+  }
 }
